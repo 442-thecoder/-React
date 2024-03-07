46348e35805ac692df78b51c68c37ae7aac64147
442-the coder <19u91a0442@gmail.com>
2024-03-07 19:15:09 +0530

    bgchanger

---
+++ bg-changer/src/App.jsx
@@ -0,0 +1,66 @@
+ /*import { useState } from 'react'
+ import './App.css'
+
+ function App() {
+  const [bg, setbg] = useState('#000000')
+  function change()
+  {
+      Math.round(Math.random() * 10000000).toString(16)
+     const random_color = "#"+Math.round(Math.random() * 10000000).toString(16)
+      setbg(random_color)
+  }
+  return (
+       <body style={{backgroundColor:bg}}>
+         <div>
+           <button onClick={change} >change</button>
+        </div>
+       </body>
+  )
+}
+export default App */
+
+ import { useState } from 'react'
+ import './App.css'
+
+ function App() {
+  const [bg, setbg] = useState('#000000')
+   function change()
+   {
+      //  Math.round(Math.random() * 10000000).toString(16)
+       const random_color = input.value
+       setbg(random_color)
+   }
+   return (
+       <body style={{backgroundColor:bg}}>
+         <div>
+            <input type="color"  onChange={change}  name="" id="input" />
+         </div>
+       </body>
+   )
+ }
+ export default App
+/*import { Component } from 'react'
+import "./App.css"
+
+export default class App extends Component {
+  constructor()
+  {
+    super()
+    this.state={background:'#000000'}
+  }
+  change = () =>
+  {
+    Math.round(Math.random() * 10000000).toString(16)
+			const random_color ="#" + Math.round(Math.random() * 10000000).toString(16)
+     this.setState(random_color)
+  }
+  render() {
+    return (
+     <body style={{ backgroundColor: this.state.background }}>
+       <div>
+        <input onChange={this.change} type="color" id="input" />
+       </div>
+     </body>
+    )
+  }
+}*/
