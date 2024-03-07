b633222e6176b27ccf4e38bd0b4939634d3c9963
442-the coder <19u91a0442@gmail.com>
2024-03-07 19:19:17 +0530

    component

---
+++ first/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import logo from './logo.svg';
+function App() {
+  return (
+    <body>
+    <div class="App-header" >
+   
+     <h1>my react js</h1>
+     <img src={logo} className="App-logo" alt="logo" />
+      <p>React is the library for web and native user interfaces. Build user interfaces out of individual pieces called components written in JavaScript.</p>
+    </div>
+    
+    </body>
+  );
+}
+
+export default App;
